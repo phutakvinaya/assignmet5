b5c1bd732adf88da87de878bda250712d0cdb8b0
Your Name <phutakvinaya@gmail.com>
2023-12-25 00:03:17 +0530

    initial commit

---
+++ src/app/firstcomp/firstcomp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondcompComponent } from '../secondcomp/secondcomp.component';
+@Component({
+  selector: 'app-firstcomp',
+  standalone: true,
+  imports: [SecondcompComponent],
+  templateUrl: './firstcomp.component.html',
+  styleUrl: './firstcomp.component.css'
+})
+export class FirstcompComponent {
+
+}
